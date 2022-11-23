33b30df9b992927936e35338c1b3da40b72afcdd
Rungphailin Samanantakun <88391247+DaoRungphailin@users.noreply.github.com>
2022-11-23 23:08:45 +0700

    Lab 10 Sorting

---
+++ Searching/2_FirstGreatValue.py
@@ -0,0 +1,22 @@
+'''ให้น้องเขียนโปรแกรมหาค่าที่น้อยที่สุดที่มากกว่าค่าที่ต้องการจะหา ถ้าหากไม่มีให้แสดงว่า No First Greater Value โดยตัวเลขของทั้ง 2 list รับประกันว่าไม่เกิน 1000000
+
+***** อธิบาย Test Case 2:
+Left : [3, 2, 7, 6, 8]         Right : [5, 6, 12]
+1. หาค่าที่น้อยที่สุดที่มากกว่า 5 จาก list (Left) จะได้เป็น 6
+2. หาค่าที่น้อยที่สุดที่มากกว่า 6 จาก list (Left) จะได้เป็น 7
+3. หาค่าที่น้อยที่สุดที่มากกว่า 12 จาก list (Left) จะเห็นว่าไม่มีค่าที่มากกว่า 12 จะแสดงเป็น No First Greater Value
+'''
+
+com = input('Enter Input : ').split('/')
+Left, Right = sorted(list(map(int, com[0].split()))), list(map(int, com[1].split()))
+
+for i in Right:
+    greaterVal = None
+    for j in Left:
+        if j > i:
+            greaterVal = j
+            break
+    if greaterVal == None:
+        print("No First Greater Value")
+    else:
+        print(j)
